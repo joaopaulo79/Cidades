78f8b88d4e2554c8cd12361e3bb69ca74fa940a9
João Paulo <joaopdejesus79@gmail.com>
2021-08-28 11:29:26 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" ");
+            Console.WriteLine("Cidades do litoral sul de São Paulo:");
+            Console.ResetColor();
+            Console.WriteLine(" ");
+            Console.WriteLine("- Praia Grande");
+            Console.WriteLine("- Mongaguá");
+            Console.WriteLine("- Itanhaém");
+            Console.WriteLine("- Peruíbe");
+            Console.WriteLine(" ");
+
+        }
+    }
+}
